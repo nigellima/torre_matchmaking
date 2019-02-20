4948e5f0167ecc79a58eca0c296cc13de9bee686
Nigel Da Silva Lima <nigelnaiguel.comp@gmail.com>
2019-02-19 23:16:02 -0300

    material ui installed properly on client

---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+function Navbar(props) {
+  const { classes } = props;
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" color="inherit" className={classes.grow}>
+            Torre Job Matchmaking
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+
+Navbar.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Navbar);
