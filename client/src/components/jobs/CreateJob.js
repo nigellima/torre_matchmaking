a005b891b7859542dbd2b3610b3235a381fca31b
Nigel Da Silva Lima <nigelnaiguel.comp@gmail.com>
2019-02-20 20:38:38 -0300

    adding contract type field

--- client/src/components/jobs/CreateJob.js
+++ client/src/components/jobs/CreateJob.js
@@ -4,6 +4,7 @@ import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import MenuItem from '@material-ui/core/MenuItem';
 
 import TextEditor from './TextEditor';
 
@@ -27,7 +28,8 @@ class NewJob extends Component {
   constructor(props){
     super(props);
     this.state = {
-      description: ''
+      description: '',
+      contract_type: ''
     }
   }
 
@@ -42,6 +44,12 @@ class NewJob extends Component {
 
   render() {
     const { classes } = this.props;
+    const types = [
+        { label: '* Select Contract Type ', value: 0 },
+        { label: 'Long-term', value: 'LONG' },
+        { label: 'Short-term', value: 'SHORT' },
+    ]
+
 
     return (
       <div className={classes.container}>
@@ -78,6 +86,32 @@ class NewJob extends Component {
               name='description'
               className={classes.textField}
             />
+            <Typography className={classes.text} variant="h5" gutterBottom>
+              Contract Type
+            </Typography>
+            <TextField
+              id="contract-type"
+              select
+              fullWidth
+              className={classes.textField}
+              value={this.state.contract_type}
+              name='contract_type'
+              onChange={this.onChange}
+              SelectProps={{
+                MenuProps: {
+                  className: classes.menu,
+                },
+              }}
+              helperText="Please select the desired contract type. 
+                          This information will be used to sort the best candidates for you"
+              margin="normal"
+            >
+              {types.map(option => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
           </Grid>   
         </Grid> 
         
